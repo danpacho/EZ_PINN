1621008c142bbf6e44a2737ac956382ea1f47c8d
Danpa_cho <danpa725@cau.ac.kr>
2024-11-04 22:41:01 +0900

    test: add get_device test script

--- src/utils/device_setup.py
+++ src/utils/device_setup.py
@@ -5,3 +5,7 @@ import torch
 def get_device() -> Union[Literal["mps"], Literal["cpu"]]:
     device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
     return device
+
+
+if __name__ == "__main__":
+    print(get_device())
