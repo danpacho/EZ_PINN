5bec7966605e21632d1e1507a2dda8d8c32dcb5c
danpacho <danpa725@cau.ac.kr>
2024-11-05 22:01:54 +0900

    docs: add experimental device

--- src/utils/device_setup.py
+++ src/utils/device_setup.py
@@ -7,5 +7,10 @@ def get_device() -> Union[Literal["mps"], Literal["cpu"]]:
     return device
 
 
+# def get_device(gpu_num: int = 3) -> Union[str, Literal["cpu"]]:
+#     device = torch.device(f"cuda:{gpu_num}" if torch.cuda.is_available() else "cpu")
+#     return device
+
+
 if __name__ == "__main__":
     print(get_device())
